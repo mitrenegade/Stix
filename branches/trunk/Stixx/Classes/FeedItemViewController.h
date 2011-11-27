e9ad65fcbb8f19c592b442ef5c16ed6a4eef0ae2
Bobby Ren <bobbyren@gmail.com>
2011-11-27 00:30:23 -0500

    added labelCommentBG; added locationString

--- branches/trunk/Stixx/Classes/FeedItemViewController.h
+++ branches/trunk/Stixx/Classes/FeedItemViewController.h
@@ -14,26 +14,31 @@
 @interface FeedItemViewController : UIViewController {
     
 	IBOutlet UILabel * labelName;
+    IBOutlet UILabel * labelCommentBG; // needed for opacity trick
 	IBOutlet UILabel * labelComment;
 	IBOutlet UILabel * labelTime;
+    IBOutlet UILabel * labelLocationString;
 	IBOutlet UIImageView * imageView;
     IBOutlet UIImageView * userPhotoView;
     
     NSString * nameString;
     NSString * commentString;
+    NSString * locationString;
     UIImage * imageData;
    
 }
 @property (retain, nonatomic) IBOutlet UILabel * labelName;
 @property (retain, nonatomic) IBOutlet UILabel * labelComment;
+@property (retain, nonatomic) IBOutlet UILabel * labelCommentBG;
 @property (retain, nonatomic) IBOutlet UILabel * labelTime;
+@property (retain, nonatomic) IBOutlet UILabel * labelLocationString;
 @property (nonatomic, retain) IBOutlet UIImageView * imageView;
 @property (nonatomic, retain) IBOutlet UIImageView * userPhotoView;
 @property (nonatomic, retain) NSString * nameString;
 @property (nonatomic, retain) NSString * commentString;
 @property (nonatomic, retain) UIImage * imageData;
 
--(void)populateWithName:(NSString *)name andWithComment:(NSString*)comment andWithImage:(UIImage*)image;
+-(void)populateWithName:(NSString *)name andWithComment:(NSString*)comment andWithLocationString:(NSString*)location andWithImage:(UIImage*)image;
 -(void)populateWithUserPhoto:(UIImage*)photo;
 -(void)populateWithTimestamp:(NSDate *)timestamp;
 -(void)populateWithBadge:(int)type withCount:(int)count atLocationX:(int)x andLocationY:(int)y;

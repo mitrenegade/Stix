cc50de4e825d8956aac376ff48408d2dbe2716af
Bobby Ren <bobbyren@gmail.com>
2011-11-27 02:56:28 -0500

    added labelCommentBG and labelLocationString; added setLocation

--- branches/trunk/Stixx/Classes/ZoomViewController.h
+++ branches/trunk/Stixx/Classes/ZoomViewController.h
@@ -20,14 +20,20 @@
     IBOutlet UIButton * imageView;
     //IBOutlet UIButton * backButton;
     IBOutlet UILabel * labelComment;
+    IBOutlet UILabel * labelCommentBG;
+    IBOutlet UILabel * labelLocationString;
 	NSObject<ZoomViewDelegate> *delegate;
 }
 @property (nonatomic, retain) IBOutlet UIButton * imageView;
 //@property (nonatomic, retain) IBOutlet UIButton * backButton;
 @property (nonatomic, retain) IBOutlet UILabel * labelComment;
+@property (nonatomic, retain) IBOutlet UILabel * labelCommentBG;
+@property (nonatomic, retain) IBOutlet UILabel * labelLocationString;
 @property (nonatomic, retain) NSObject<ZoomViewDelegate> *delegate;
+@property (nonatomic, retain) ZoomViewController * zoomViewController;
 //@property (nonatomic, retain) UIImage * image;
 -(IBAction)didPressBackButton:(id)sender;
 -(void)forceImageAppear:(UIImage*)img;
 -(void)setLabel:(NSString*)label;
+-(void)setLocation:(NSString *)location;
 @end
